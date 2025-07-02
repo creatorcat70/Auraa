a9ce7c1f8f69aa456e34b3d8e636f8fea796a3ff
Sydney <faarisfarhan66@gmail.com>
2025-07-02 16:57:38 -0700

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://bare.soloo.fun/',
+    bare:'http://bare.soloo.fun/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
