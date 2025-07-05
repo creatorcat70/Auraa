6811ddd5f71af70986bbd56fe967d9da060d29a0
Sydney <faarisfarhan66@gmail.com>
2025-07-04 19:41:12 -0700

    Add files via upload

---
+++ public/active/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/active/go/',
+    bare:'https://math.cinevez.lol/bare/',
+    encodeUrl: Ultraviolet.codec.base64.encode,
+    decodeUrl: Ultraviolet.codec.base64.decode,
+    handler: '/active/uv/uv.handler.js',
+    bundle: '/active/uv/uv.bundle.js',
+    config: '/active/uv/uv.config.js',
+    sw: '/active/uv/uv.sw.js',
+};
